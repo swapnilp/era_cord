0a79ee058fee04b70ea5f1608f8888af9d74510d
SwapnilPatil <swapnil.patil04@gmail.com>
2016-03-17 22:43:35 +0530

    worke on UI

--- app/serializers/jkci_class_index_serializer.rb
+++ app/serializers/jkci_class_index_serializer.rb
@@ -1,9 +1,9 @@
 class JkciClassIndexSerializer < ActiveModel::Serializer
   #attributes :id, :name, :assign_to, :actions, :last_login, :standard_id, :is_selected
-  attributes :id, :class_name, :divisions, :remaining_activity, :organisation_id, :is_student_verified
+  attributes :id, :class_name, :students, :remaining_activity, :organisation_id, :is_student_verified
 
-  def divisions
-    object.sub_classes.count
+  def students
+    object.class_students_count
   end
   
   def remaining_activity
