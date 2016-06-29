df010e4ac6a7fcf23fcf60f63d401e69a8d9a1f4
SwapnilPatil <swapnil.patil04@gmail.com>
2016-06-29 20:10:23 +0530

    work on teachers

--- app/models/teacher_subject.rb
+++ app/models/teacher_subject.rb
@@ -1,4 +1,11 @@
 class TeacherSubject < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :subject
+
+  def as_json(options = {})
+    options.merge({
+                    id: id,
+                    subject_name: subject.std_name
+                  })
+  end
 end
