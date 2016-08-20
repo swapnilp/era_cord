e7c937dcc833535ae5051192e13fd03bcc0b9365
SwapnilPatil <swapnil.patil04@gmail.com>
2016-08-20 15:28:35 +0530

    work on mobile api

---
+++ app/controllers/api/v1/time_table_classes_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::TimeTableClassesController < ApiController
+  before_action :authenticate_user!
+
+  def index
+    teacher = current_user.teacher 
+    return render json: {success: false, message: "Invalid teacher"} unless teacher
+    time_table_classes = teacher.time_table_classes
+    render json: {success: true, time_table_classes: time_table_classes.map(&:teacher_json)} 
+  end
+
+  def get_chapters
+    teacher = current_user.teacher 
+    return render json: {success: false, message: "Invalid teacher"} unless teacher
+    time_table_class = teacher.time_table_classes.where(id: params[:id]).first
+    if time_table_class.present?
+      chapters = time_table_class.subject.chapters.select([:id, :name, :chapt_no])
+      render json: {success: true, chapters: chapters.as_json} 
+    else
+      render json: {success: false, message: "Invalid Class"}
+    end
+  end
+
+  def get_chapters_point
+    teacher = current_user.teacher 
+    return render json: {success: false, message: "Invalid teacher"} unless teacher
+    time_table_class = teacher.time_table_classes.where(id: params[:id]).first
+    chapter = Chapter.where(id: params[:chapter_id]).first
+    if time_table_class.present? && chapter.present?
+      points = chapter.chapters_points.select([:id, :name, :point_id])
+      render json: {success: true, points: points.as_json} 
+    else
+      render json: {success: false, message: "Invalid Class"}
+    end
+  end
+end
