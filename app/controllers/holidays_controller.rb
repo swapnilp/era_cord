7ece3a9b9635337f852ca1d1befbf24c7773fbf2
SwapnilPatil <swapnil.patil04@gmail.com>
2017-01-18 17:45:14 +0530

    work on holiday

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -20,18 +20,21 @@ class HolidaysController < ApplicationController
   def create
     return render json: {success: false, message: "Must be root user"} unless @organisation.root?
     
-    holiday = Holiday.new(create_params.merge({organisation_id: @organisation.id, is_goverment: false}))
-    if holiday.save
+    if create_params[:isMultiDate]
+      (JSON.parse(params['dateRange'])['startDate'].to_time.to_date..JSON.parse(params['dateRange'])['endDate'].to_time.to_date ).to_a.each do |date| 
+        Holiday.new({date: date, reason: create_params[:reason], organisation_id: @organisation.id, is_goverment: false}).save
+      end
       render json: {success: true}
     else
-      render json: {success: false}
+      Holiday.new(create_params.merge({organisation_id: @organisation.id, is_goverment: false})).save
+      render json: {success: true}
     end
   end
     
   private
 
   def create_params
-    params.require(:holiday).permit(:date, :reason)
+    params.require(:holiday).permit(:date, :reason, :isMultiDate)
   end
 
   def update_params
--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,5 +1,7 @@
 class Holiday < ActiveRecord::Base
   belongs_to :organisation
+
+  attr_accessor :isMultiDate, :dateRange
   
   default_scope { where("organisation_id in (?)", [0, Organisation.current_id].flatten.compact) }  
 end
