7aa1e194aef39d332c80ee4176340986f80b4af4
Ubuntu <ubuntu@ip-172-31-15-50.ec2.internal>
2017-06-24 07:17:16 +0000

    work on delayed job tenant

---
+++ config/initializers/delayed_job.rb
@@ -0,0 +1,30 @@
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
+Delayed::Worker.logger.debug("Log Entry")
+
+module Delayed
+  module Plugins
+    class Organisationwise < Plugin
+      @prev_organisation = nil
+      callbacks do |lifecycle|
+        lifecycle.before(:invoke_job) do |job|
+          @prev_organisation = Thread.current[:organisation_id]
+          #Thread.current[:tenant_id] = job.tenant_id                                                                                                                                                              
+          Organisation.set_current_organisation(job.organisation_id)
+        end
+        lifecycle.after(:invoke_job) do |job|
+          Thread.current[:organisation_id] = @prev_organisation
+        end
+      end
+    end
+  end
+end
+
+Delayed::Worker.plugins << Delayed::Plugins::Organisationwise
+
+Delayed::Job.class_eval do
+  #attr_accessible :organisation_id
+  before_create :add_organisation
+  def add_organisation
+     self.organisation_id = Thread.current[:organisation_id]
+   end
+end
